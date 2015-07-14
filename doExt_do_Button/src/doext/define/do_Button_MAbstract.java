815468bee0ee54a92720fab1ecefcb503798c68e
maorui <maoruiily@163.com>
2015-07-14 17:09:15 +0800

    字体相关组件属性的修改与增加 http://jira.deviceone.net:8088/browse/PRODDEV-1713

--- doExt_do_Button/src/doext/define/do_Button_MAbstract.java
+++ doExt_do_Button/src/doext/define/do_Button_MAbstract.java
@@ -20,6 +20,7 @@ public abstract class do_Button_MAbstract extends DoUIModule {
 		this.registProperty(new DoProperty("bgImage", PropertyDataType.String, "", false));
 		this.registProperty(new DoProperty("fontColor", PropertyDataType.String, "000000FF", false));
 		this.registProperty(new DoProperty("fontSize", PropertyDataType.Number, "9", false));
+		this.registProperty(new DoProperty("textFlag", PropertyDataType.String, "normal", true));
 		this.registProperty(new DoProperty("fontStyle", PropertyDataType.String, "normal", false));
 		this.registProperty(new DoProperty("radius", PropertyDataType.Number, "0", true));
 		this.registProperty(new DoProperty("text", PropertyDataType.String, "", false));
