dd2504ba5563a6f77f9b295db1733c1e81440297
zhuozy <zhuozuoying@163.com>
2015-09-10 10:04:04 +0800

    update

--- doExt_do_Button/src/dotest/module/frame/debug/DoPageViewFactory.java
+++ doExt_do_Button/src/dotest/module/frame/debug/DoPageViewFactory.java
@@ -17,16 +17,23 @@ public class DoPageViewFactory implements DoIPageViewFactory {
 		// TODO Auto-generated method stub
 		return currentActivity;
 	}
-
-	@Override
-	public void openPage(String arg0, String arg1, String arg2, String arg3,
-			String arg4, String arg5, String arg6, String arg7) {
-		// TODO Auto-generated method stub
-
-	}
 	
 	public void setCurrentActivity(Activity currentActivity) {
 		this.currentActivity = currentActivity;
+	}
+
+	@Override
+	public void openPage(String _pageID, String _appID, String _uiPath,
+			String _scriptFile, String _animationType, String _data,
+			String _isFullScreen, String _keyboardMode, String _callbackFuncName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closePage(String _animationType, String _data, String _id) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
